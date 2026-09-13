c05bad2cbf48c1b162a4175ba1d61a4c0104c968
Noah Ellis <nellis1@buffs.wtamu.edu>
2026-09-13 11:45:09 -0500

    Register models and add catalog data

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Book, Publisher, Review
+
+
+admin.site.register(Publisher)
+admin.site.register(Book)
+admin.site.register(Review)
